6afe953cf8284aa2f289e240ced7188fab87d4ef
kunal-vichare <kunalvichare9999@gmail.com>
2026-03-16 14:34:55 +0530

    Add UI of sort & fileter button

--- src/components/HomeScreen/SortandFilter.jsx
+++ src/components/HomeScreen/SortandFilter.jsx
@@ -1,37 +1,106 @@
-import { View, Text, TouchableOpacity, StyleSheet ,Modal} from 'react-native'
+import { View,Text, TouchableOpacity, StyleSheet} from 'react-native'
 import React,{ useState } from 'react'
-// import React from 'react'
 import Sort from 'react-native-vector-icons/FontAwesome';
 import Filter from 'react-native-vector-icons/Ionicons';
+import {Menu, Divider,Modal, Portal} from 'react-native-paper';
+import { Dropdown } from 'react-native-paper-dropdown';
+
 
 const SortandFilter = () => {
-  const[modalVisible,setModalVisible] = useState(false);
+  const [upload, setUpload] = useState('');
+  const [status, setStatus] = useState('');
+  const [modalVisible,setModalVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const statusList = [
+    { label: "Active", value: "active"},
+    { label: "Inactive", value: "inactive"},
+  ];
+  const uploadList = [
+    { label: "Anytime", value: "anytime"},
+    { label: "Today", value: "today"},
+    { label: "This week", value: "this week"},
+    { label: "This month", value: "this month"},
+    { label: "This year", value: "this year"},
+  ];
+
   return (
     <View style={styles.btnContainer}>
-      <TouchableOpacity style={styles.sortBtn}>
-        <Text style={styles.sortText}>Sort by</Text>
-        <Sort name="unsorted" size={24}/>
-      </TouchableOpacity>
-      <TouchableOpacity 
-        onPress={() => setModalVisible(true)}
-        style={styles.filterBtn}
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={
+            <TouchableOpacity 
+            onPress={openMenu}
+            style={styles.sortBtn}
+            >
+              <Text style={styles.sortText}>
+                Sort by
+              </Text>
+              <Sort name="unsorted" size={24}/>
+            </TouchableOpacity>
+          }>         
+          <Menu.Item onPress={() => {}} title="Name" />
+          <Menu.Item onPress={() => {}} title="Time" />
+        </Menu>
+
+        <TouchableOpacity 
+          onPress={openModal}
+          style={styles.filterBtn}
         >
-        <Text style={styles.filterText}>Filter</Text>
-        <Filter name="filter-sharp" size={24} />
-      </TouchableOpacity>
-      <Modal 
-      visible={modalVisible}
-      transparent={true}
-      animationType='fade'
-      onRequestClose={()=>setModalVisible(false)}
-      >
-        <View>
-          <Text>This is modal</Text>
-          <TouchableOpacity onPress={()=>setModalVisible(false)}>
-            <Text>Close</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+          <Text style={styles.filterText}>Filter</Text>
+          <Filter name="filter-sharp" size={24} />
+        </TouchableOpacity>
+
+      <Portal>
+        <Modal visible={modalVisible} onDismiss={closeModal} contentContainerStyle={styles.modalContainer}>
+          <View style={styles.filterContainer}>
+            <View style={styles.headerContainer}>
+            <Text style={styles.filterHeader}>Search filters</Text>
+            </View>
+            <View style={styles.filterFieldsContainer}>
+              <Text style={styles.filterText}>Status</Text>
+              <View style={styles.dropdownContainer}>
+                <Dropdown
+                  placeholder="Select status"
+                  options={statusList}
+                  value={status}
+                  onSelect={setStatus}
+                  menuContentStyle={styles.menu}
+                  style={styles.dropdown}
+                />
+              </View>
+            </View>
+            <View style={styles.filterFieldsContainer}>
+              <Text style={styles.filterText}>Upload date</Text>
+              <View style={styles.dropdownContainer}>
+                <Dropdown
+                  placeholder="Select upload date"
+                  options={uploadList}
+                  value={upload}
+                  onSelect={setUpload}
+                  menuContentStyle={styles.menu}
+                  style={styles.dropdown}
+                />
+              </View>
+            </View>
+            <Divider style={{marginBottom : 20}}/>
+              <View style={styles.filterFieldsContainer}>
+              <TouchableOpacity style={styles.cancelBtn} onPress={closeModal}>
+                <Text style={styles.cancelText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.applyBtn}>
+                <Text style={styles.applyText}>Apply</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </Portal>
     </View>
   )
 }
@@ -39,33 +108,79 @@ const SortandFilter = () => {
 const styles = StyleSheet.create({
   btnContainer : {
     flexDirection : 'row',
-    // backgroundColor:'red',
     alignItems: 'center',
     justifyContent:'center'
   },
   sortText : {
     fontSize: 15,
     fontWeight: 'bold',
-    marginRight:5
+    marginRight:10
   },
-  sortBtn : {
-    flexDirection:'row',
-    // backgroundColor: '#e1fcfc',
-    marginRight: 5,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  filterText : {
-    marginLeft : 10,
-    fontSize: 15,
-    fontWeight: 'bold',
-    marginRight:5
+    filterText : {
+    fontWeight : 'bold',
+    fontSize : 15,
+    marginRight : 10
   },
   filterBtn : {
      flexDirection:'row',
-    //  backgroundColor:'#e1fcfc',
      marginTop: 10,
      marginBottom: 10,
+  },
+    sortBtn : {
+    flexDirection:'row',
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 15,
+  },
+  modalContainer : {
+    backgroundColor: 'white', 
+    padding: 20,
+  },
+  filterContainer : {
+    flexDirection: 'column',
+  },
+  filterFieldsContainer : {
+    flexDirection : 'row',
+    marginBottom : 45
+  },
+
+  filterHeader : {
+    fontSize : 23,
+    fontWeight : '600'
+  },
+  headerContainer : {
+    marginBottom : 30
+  },
+  dropdownContainer : {
+    position : 'absolute',
+    left : 140,
+    width:230,
+  },
+  menu : {
+
+  },
+  cancelBtn : {
+    backgroundColor : '#fdc8c8',
+    padding : 15,
+    borderRadius : 15,
+    marginLeft : 100,
+    marginRight : 30,
+  },
+  applyBtn : {
+    backgroundColor : '#c8f6fd',
+    padding : 15,
+    borderRadius : 15
+  },
+  cancelText : {
+    fontSize : 15,
+    fontWeight : 'bold'
+  },
+  applyText : {
+    fontSize : 15,
+    fontWeight : 'bold'
+  },
+  dropdown : {
+    
   }
 })
 
